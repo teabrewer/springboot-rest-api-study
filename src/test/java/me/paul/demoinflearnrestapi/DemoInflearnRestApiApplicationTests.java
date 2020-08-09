a606309203d062bff186a5d3b7930111d0aac2a6
paul <paul@within.co.kr>
2020-08-10 01:38:24 +0900

    event domain

---
+++ src/test/java/me/paul/demoinflearnrestapi/DemoInflearnRestApiApplicationTests.java
@@ -0,0 +1,13 @@
+package me.paul.demoinflearnrestapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoInflearnRestApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
---
+++ src/test/java/me/paul/demoinflearnrestapi/events/EventTest.java
@@ -0,0 +1,33 @@
+package me.paul.demoinflearnrestapi.events;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class EventTest {
+
+    @Test
+    public void builder() {
+        Event event = Event.builder()
+                .name("Test Spring REST API By Paul")
+                .description("REST API development with Spring")
+                .build();
+        assertThat(event).isNotNull();
+    }
+
+    @Test
+    public void javaBean() {
+        //Give
+        String name = "Event";
+        String description = "Spring";
+
+        //When
+        Event event = new Event();
+        event.setName(name);
+        event.setDescription(description);
+
+        //Then
+        assertThat(event.getName()).isEqualTo(name);
+        assertThat(event.getDescription()).isEqualTo(description);
+    }
+}
